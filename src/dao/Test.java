aae06950a707cf5ecc7fed2a635a7e59d7fb2cea
fxdy <3030384958@qq.com>
2023-12-11 22:33:42 +0800

    将数据库连接修改为mysql

--- src/dao/Test.java
+++ src/dao/Test.java
@@ -5,24 +5,24 @@ import java.sql.DriverManager;
 
 public class Test {
 
-//	public static void main(String[] args)
-//	{
-//		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-//		String dbURL = "jdbc:sqlserver://localhost:1433;DatabaseName=db_Ebook;encrypt=true;trustServerCertificate=true";
-//		String userName = "sa";
-//		String userPwd = "Password1234567890";
-//		Connection dbConn = null;
-//		try
-//		{
-//			Class.forName(driverName);
-//			System.out.println("连接数据库...");
-//			dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
-//			System.out.println("连接数据库成功");
-//		}
-//		catch (Exception e)
-//		{
-//			e.printStackTrace();
-//			System.out.print("连接失败");
-//		}
-//	}
+	public static void main(String[] args)
+	{
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		String dbURL = "jdbc:mysql://localhost:3306/E_books?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+		String userName = "root";
+		String userPwd = "123456";
+		Connection dbConn = null;
+		try
+		{
+			Class.forName(driverName);
+			System.out.println("连接数据库...");
+			dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+			System.out.println("连接数据库成功");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.out.print("连接失败");
+		}
+	}
 }
